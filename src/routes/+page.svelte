5d142237c729f80dd8465379d550d47d849dbf86
Marco Antonio Vasquez Pacheco <100816424+Antonio3213@users.noreply.github.com>
2024-03-15 17:20:27 -0700

    firstCommit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,27 @@
 <h1 class="text-3xl font-bold underline">
     this is the simple main
 </h1>
+
+<div>
+    <ul>
+        <li>
+            <a href="">
+                login
+            </a>
+        </li>
+        <li>
+            <a href="../app/home">
+                home
+            </a>
+        </li>
+        <li>
+            <a href="">
+                
+            </a>
+        </li>
+    </ul>
+    
+
+</div>
   
-<ul>
-    <li>
-        <a href="">
-            login
-        </a>
-    </li>
-    <li>
-        <a href="../app/home">
-            home
-        </a>
-    </li>
-    <li>
-        <a href="">
-            
-        </a>
-    </li>
-</ul>
 
